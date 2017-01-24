5ba469d4d6b3bdc6d44e17c33302a948919e3ca5
Maddie Phillips <Madisen2@pdx.edu>
2017-01-24 08:45:25 -0800

    Second Push

--- create_jpeg.py
+++ create_jpeg.py
@@ -1,6 +1,10 @@
 from PIL import Image
 im=Image.open("chris.jpg")
 
+# :set tabstop=4 softtabstop=4 expandtab shiftwidth=4 autoindent
+# :set list
+# :%s/\t/    /g
+
 print(im.format,im.size,im.mode)
 
 im.show()
@@ -8,23 +12,23 @@ im.show()
 import os, sys
 
 for infile in sys.argv[1:]:
-	f, e = os.path.splitext(infile)
-	outfile = f + ".jpg"
-	if infile != outfile:
-		try:
-			Image.open(infile).save(outfile)
-		except IOError:
-			print("cannot convert", infile)
+    f, e = os.path.splitext(infile)
+    outfile = f + ".jpg"
+    if infile != outfile:
+        try:
+            Image.open(infile).save(outfile)
+        except IOError:
+            print("cannot convert", infile)
 
 
 for infile in sys.argv[1:]:
-	try:
-		with Image.open(infile) as im:
-			print(infile, im.format, "%dx%d" % im.size, im.mode)
+    try:
+        with Image.open(infile) as im:
+            print(infile, im.format, "%dx%d" % im.size, im.mode)
 
 box = (100, 100, 400, 400)
 
 region = im.crop(box)
 
-	except IOError:
-		pass
+    except IOError:
+        pass
