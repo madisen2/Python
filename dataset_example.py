312c279e0a080b493eaedd1ca14ebd38f1e9b033
Maddie Phillips <madisen2@titlis.ece.pdx.edu>
2017-08-08 15:54:06 -0700

    This is the base version of my script thats working

--- dataset_example.py
+++ dataset_example.py
@@ -1,6 +1,4 @@
-"""Script usage:
-
-# To show the 0th training image with its label data
+"""Script usage:To show the 0th training image with its label data
 python dataset_example.py show 0
 python dataset_example.py count
 """
@@ -56,7 +54,7 @@ def get_image(n,is_test):
     """Returns the nth image from the training or test set.
     Should return the image as an array of floats."""
 
-    subject = Image.open((get_imlist(is_test))[n]).convert("RGB")
+    subject = Image.open((get_imlist(is_test))[n]).convert("L")
     subject_array=np.asarray(subject,dtype=np.float32)
 
     return subject_array/255
@@ -87,11 +85,9 @@ def get_target(n):
 
 #    mask=Image.open(str(get_imlist(n)+'mask.tif').convert("RGB")
 
-    mask=Image.open(REAL_PATH_TRAIN+'/'+os.path.basename((get_imlist(False)[n])).split('.')[0]+'_mask.tif').convert("RGB")
-
-   # mask=Image.open(REAL_PATH_TRAIN+'/'+
-   #str(get_subject(n))+'_'+str(n)+'_mask.tif').convert("RGB")
-    mask_array=np.array(mask, dtype='float32')
+    mask=Image.open(REAL_PATH_TRAIN+'/'+os.path.basename((
+        get_imlist(False)[n])).split('.')[0]+'_mask.tif').convert("L")
+    mask_array=np.asarray(mask,dtype=np.float32)
 
     return mask_array/255
 
@@ -138,101 +134,6 @@ def demo_show(image):
 #________________________________________________________________________#
 
 
-REAL_PATH_TEST="/u/madisen2/Python/Nerve_Segmentation/test"
-REAL_PATH_TRAIN="/u/madisen2/Python/Nerve_Segmentation/train"
-
-
-
-def get_imlist(path):
-
-    """Returns a list or filenames for all tif images in a directory."""
-    imlist=get_imlist(path)
-
-     return [os.path.join(path,f) for f in os.listdir(path) if f.endswith('.tif')
-
-
-
-
-def count_frames(is_test):
-    """This function should return the number of sample frames
-    in either the training set (is_test == False) or test set
-    (is_test == True).
-    """
-
-    if is_test:
-
-        images=get_imlist(REAL_PATH_TEST)
-
-    else:
-        images=get_imlist(REAL_PATH_TRAIN)
-
-
-    im=array(Image.open(images[0])) #open one image to get size
-
-    im_h,im_w=im.shape[0:2] #get the size of the images
-
-    imnbr=len(images) #get the number of images
-
-    #create matrix to store all flattened images
-    immatrix=array([array(Image.open(im)).flatten()
-            for im in images],'f'
-
-    return 0
-
-
-def get_image(is_test, n):
-    """Returns the nth image from the training or test set.
-
-    Should return the image as an array of floats.
-    """
-    # im_w is image width, im_h is image height.  The number of
-    # channels is 1 because the images are grayscale.
-    if is_test:
-        im_sort=natsorted(REAL_PATH_TEST)
-    else:
-        im_sort=natsorted(REAL_PATH_TRAIN)
-
-    image = Image.open(im_sort)
-    image.load()
-    data = np.asarray ( image, dtype="int32" )
-    return data
-
-def get_subject(n):
-    """For the training set, returns the subject ID number
-    (training set images are named subject_imageNum.tif).
-    """
-    subject_ID=os.path.basename(im_sort).split('_')[0]
-
-    """uses the same filename as get_image"""
-
-    raise NotImplementedError()
-    return 0
-
-
-def get_target(n):
-    """Returns the target for the nth image from the training set.
-
-    Note that you do not have targets from the test set.
-
-    Should return an array of bytes.
-    """
-"""need to load the mask data"""
-
-    return np.zeros((im_h, im_w, 1), dtype=np.uint8)
-
-## Demo functions
-def demo_combine(image, target):
-    """Blend target into image to highlight the target region.
-    """
-    image=Image.fromarray(data, 'RGB')
-    image = Image.composite(image,target)
-
-
-
-def demo_show(image):
-    """Shows the image.
-    """
-    image.show()
 
 @click.group()
 def main(**kwargs):
--- random_mask.py
+++ random_mask.py
@@ -5,29 +5,32 @@
 """This file is used to generate random masks to test the least squares error
 between the result and given mask data."""
 
-import os
-import click
+
+
+import os,click,PIL, numpy as np, natsort,PIL.ImageOps,dataset_example
+import matplotlib.pyplot as plt, math, tkinter
 from PIL import Image
-import numpy as np
-from natsort import natsorted
-import  PIL.ImageOps
-import dataset_example
-import matplotlib.pyplot as plt
-import math
+
+def init_ipython():
+    return {
+            'np': np,
+            'os': os,
+            }
+
 #import scipy.stats as stats
 #Utility Functions
 #____________________________________________________________________#
 
-def get_test(n):
+def get_random_array():
     """This function takes a real image target and creates an array
     of random floats (between 0 and 1) of the same size"""
 
     #loading a mask from the training set to get initial data
-    real_mask=dataset_example.get_target(n)
+    real_mask=dataset_example.get_target(get_random())
 
     #finding the size of the initial image
-    size=np.shape(real_mask)
-
+   # size=np.shape(real_mask)
+    size=(420,580)
     #creating an array of random numbers between 0 and 1
     random_array=np.random.uniform(0,1,(size))
 
@@ -40,16 +43,20 @@ def get_random():
 
     range=dataset_example.count_frames(False)#amount of trianing images
     n=np.random.randint(range)#n is a random image # in rangeof images
+    n=43
+    return n
+#___________________________________________________________________#
 
-    #loading a random array
-    random_array=get_test(n)
+
+def get_real_mask():
 
     #loading a real, but randomly chosen mask
-    real_mask=dataset_example.get_target(n)
+    real_mask=dataset_example.get_target(get_random())
 
-    return random_array,real_mask
+    return real_mask
 
 #_____________________________________________________________________#
+
 def get_dice(random_array,real_mask,threshold):
     """This function takes the dice coefficient of the test array and the
     real image target array, where real target image array is an array
@@ -61,7 +68,6 @@ def get_dice(random_array,real_mask,threshold):
     #creating an array that can be used as a threshold
     athresh[:]=0
     athresh[random_array > threshold] = 1
-    print(athresh)
 
     #function to compare pixel-wise agreement between a and b.
     #When both a and b are empty the Dice coefficient is 1.
@@ -72,30 +78,30 @@ def get_dice(random_array,real_mask,threshold):
     return dice
 #____________________________________________________________________#
 
-def get_plot(N):
+def get_plot(random_array,real_mask,N):#N):
     """This function plots the result of the dice coefficient vs. the
     threshold."""
 
-    thresholds=10
+    thresholds=1000
     #setting threshold values, starting small, can manipulate later
-    threshold=np.linspace(0.0,1.0,num=thresholds)#set this to any frequency 0-1
-
-    #plotting threshold values for error checking
-    #print(threshold)
+    threshold=np.linspace(-0.5,0.5,num=thresholds)#set this to any frequency 0-1
 
     #initializing loop variables
     count=0
     dice=[]
 
+    #initializing function variables
+    #real_mask=get_real_mask()
+    #random_array=get_random_array()
+
     while (count<N):
-        #each N gets it's own random_array and it's own real_mask
-        random_array,real_mask=get_random()
+
         dice.append([])#priming the dice array to store values
 
         for i in threshold:
-            print(i)#error checking
             dice[count].append(get_dice(random_array,real_mask,i))
         count=count+1
+
     print(dice)
 
 
@@ -114,121 +120,206 @@ def get_plot(N):
         stds.append(dice[:,count].std())
         count=count+1
 
-    print(means)
-
-
-    """
-
-
-    #initializing loop variables
-    count=0
-
-    while (count<N):
-        std=D[count,:].std()
-        plt.errorbar(threshold,D[count,:],markersize=3,fmt='o')
-
-        count=count+1
-
-
-   # plt.errorbar(threshold,D[count,:],markersize=3,fmt='o')
-   """
-
+    #plotting with caps on the standard deviation error bars
     (_, caps, _)=plt.errorbar(threshold,means,yerr=stds)
 
     #labels for plotting
+
     plt.xlabel('Threshold (with linspace)')
     plt.ylabel('Dice Coefficient')
     plt.title('Threshold v. Dice Coefficient with '+str(N)+' images')
     plt.show()
 
-
     return dice
+#_____________________________________________________________________#
+
+def get_subject(n):
+    subject_image=dataset_example.get_image(n,False)
+
+    return subject_image
+
+#____________________________________________________________________#
+
+
+def get_subject_mask(n):
+    subject_mask=dataset_example.get_target(n)
+
+    return subject_mask
+
 #______________________________________________________________________#
 
-def get_squares():
+def make_image():
 
-    #create a random array for x
-    x=np.array((np.random.randint(10,size=10))/10)
-
-    #load a random mask (x), and real given mask (y)
-     x,y=get_random()
-    #print(x.size)
-
-    #create an equal length array of equal values of y
-    #y=np.linspace(0,1,num=len(x))
-
-    #create a coefficient matrix
-    A=np.vstack([x,np.ones(len(x))]).T
-
-    #solve for slope and intercept
-    m,c=np.linalg.lstsq(A,x)[0]
-    #print slope and intercept
-    print(m,c)
-
-    plt.figure()
-    #plot original x & y
-    plt.plot(x,y,'o', label='Original data',markersize=10)
-
-    #create an empty array for what will be the best fit line
-    e=[]
-
-    #create a best fit line of random data
-    for i in x:
-        e.append(m*i+c)
-
-   # plt.figure()
-    #create a plot with error bars to best fit line
-    #plt.errorbar(y,x,e, marker='^')
-
-    #labels
-    plt.xlabel('Threshold (with linspace)')
-    plt.ylabel('Random Ints')
-    plt.title('Error bars with random ints')
+    image=np.zeros((420,580))
+    image[0:210,0:290]=1
+    image[210:420,290:580]=1
 
 
-    plt.plot(x,m*x+c,'r',label='Fitted line')
-    plt.legend()
-    plt.show()
+    return image
+
+#______________________________________________________________________#
+
+def make_mask():
+
+    mask=np.zeros((420,580))
+    mask[0:210,0:290]=1
+    mask[210:420,290:580]=1
+
+    return mask
 
 
 
+#___________________________________________________________________#
+
+def get_padding(image_pad):
+
+    """This function pads images with zeros on all sides"""
+
+    image_pad=np.pad(image_pad,(10,10),'constant',constant_values=0)
+
+    return image_pad
 
 
+#_____________________________________________________________________#
+
+def get_rc(n):
+
+    subject_image=get_subject(n)
+    rows=np.linspace(0,len(subject_image)-1,len(subject_image))
+    cols=np.linspace(0,len(subject_image[0])-1,len(subject_image[0]))
+    rows=rows.astype(int)
+    cols=cols.astype(int)
+
+    return rows, cols
+
+#____________________________________________________________________#
 
 
-   # a=dataset_example.get_target(n)
-   # b=get_test(n)
+def get_b(n):#(subject_image,subject_mask):#n):
 
-   # h,w=a.shape
-   # print(h,w)
+    #loading the images that will be used in least squares method
+    subject_image=get_subject(n)
+    subject_mask=get_subject_mask(n)
 
-   # a.flatten()
-   # print(a.shape)
+    #padding the images
+    subject_image=get_padding(subject_image)
+    subject_mask=get_padding(subject_mask)
 
-   # h,w=a.shape
-   # b.flatten()
-   # print(h,w)
-   # b=np.vstack([b,np.ones(len(b))]).T
-   # print(b.size)
-   # print(a.size)
-   # a=np.array([0.1,0.2,0.3,0.4,0.5])
+    #loop variables
+    rows,cols=get_rc(n)#subject_image)#n)
+    b=[]
+    for i in rows:
+        for j in cols:
+            if (1==subject_mask[i,j].mean()):
+                b.append(1)
+            else:
+                b.append(0)
 
-   # b=np.array([0.2,0.6,0.8,0.3,0.1])
-   # a=np.vstack([a,np.ones(len(a))]).T
-   # print(a.size)
-   # m,c=np.linalg.lstsq(a,b)[0]
+    b=np.asarray(b)
+    return b
+#______________________________________________________________________#
+
+
+def get_squares(areas,b):
+
+    least_squares=np.linalg.lstsq(areas,b)
+
+
+    return least_squares
+
+#_____________________________________________________________________#
+
+def get_areas(n):
+
+    #loading and padding the image
+    subject_image=get_subject(n)
+    subject_image=get_padding(subject_image)
+
+    #setting loop varibles
+    rows,cols=get_rc(n)
+    areas=[]
+
+    for i in rows:
+        for j in cols:
+            areas.append(subject_image[i:i+21,j:j+21])
+
+    length=len(areas)
+    count=0
+
+    while count<length:
+        areas[count]=areas[count].flatten()
+        count=count+1
+
+    return areas#np.asarray(areas)
+#_____________________________________________________________________#
+
+def get_estimated(subject_image,subject_mask):#n):
+
+
+    x_b=get_b(subject_image,subject_mask)#n)
+    areas=get_areas(subject_image)#n)
+    coefficients=get_squares(areas,x_b)[0]
+    b_new=[]
+    count=0
+
+
+    while count<len(areas):#dataset_example.count_frames(False):#5636:#243600:
+
+        b_new.append((coefficients*areas[count]).sum())
+        count=count+1
+
+    b_new=np.asarray(b_new)
+    b_new=b_new/(b_new.max())
+
+    count=0
+
+#    while count<len(b_new):
+ #       if b_new[count]<0:
+  #          b_new[count]=0
+   #     count=count+1
+   # count=0
+
+
+    b_new=np.reshape(b_new,(420,580))
+   # temp=Image.fromarray(np.asarray(b_new*255,dtype=np.uint8),"L").show()
+
+    return b_new
+
+#______________________________________________________________________#
+
+def stitch(mask,estimated_mask):
+
+    mask=Image.fromarray(np.asarray(mask*255,dtype=np.uint8),"L")
+    estimated_mask=Image.fromarray(np.asarray(estimated_mask*255,dtype=np.uint8),"L")
+    mask.save("mask.png")
+    estimated_mask.save("estimated_mask.png")
+    image1=Image.open("mask.png")
+    image2=Image.open("estimated_mask.png")
+
+    (width1,height1)=image1.size
+    (width2,height2)=image2.size
+
+    result_width=width1+width2
+    result_height=max(height1,height2)
+
+    result=Image.new("L",(result_width,result_height))
+    result.paste(im=image1,box=(0,0))
+    result.paste(im=image2,box=(width1,0))
+    result.show()
+    result.save("stitch.png")
+
     return 0
-#______________________________________________________________________#
 
 
+#_______________________________________________________________________#
 
 
+    array=np.asarray([0,1,0,1])
+    array=np.reshape(array,(2,2))
 
+    return array
 
-
-
-
-
+#_________________________________________________________________________
 
 
 
